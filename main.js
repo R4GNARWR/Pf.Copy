94e657f875f64b0d64e570e2cf2d551a0bb83ee7
R4GNAR <kirill.asylbaev@gmail.com>
2022-10-14 16:54:21 +0300

    sideMenu raw

--- main.js
+++ main.js
@@ -4,12 +4,85 @@ $(document).ready(function() {
 
     $(".overview__grid").niceScroll({
         cursorborder:"",cursorcolor:"#00F",
-        oneaxismousemode: false,
-        touchbehavior: true,
+        emulatetouch: true,    
         autohidemode: false,
-        preventmultitouchscrolling: false,
+        preventmultitouchscrolling: true,
         cursordragontouch: true,
-        cursordragspeed: '.1',
+        cursordragspeed: '0.3',
     }); // First scrollable DIV
+    $sideMenu = $('#sideMenu');
+    
+    $dataFloor = '';
+    $dataFlatNumber = '';
+    $dataPorch = '';
+    $dataPrice = '';
+    $dataPriceSq = '';
+    $dataHouse = '';
+    $dataJk = '';
+    $dataRooms = '';
+    $dataSq = '';
+    $dataCond = '';
+    $condFree = '<span class="legend-item__color legend-item__color-free"></span> Свободная';
+    $condBooked = '<span class="legend-item__color legend-item__color-reserved"></span> Забронированная';
+    $condSelled = '<span class="legend-item__color legend-item__color-sold"></span> Проданная';
+    $condDis = '<span class="legend-item__color legend-item__color-disabled"></span> Недоступна';
 
 });
+
+function insertData(block, btn)
+{
+    $dataFloor = btn.dataset.floor;
+    $dataFlatNumber = btn.dataset.flatNumber;
+    $dataPorch = btn.dataset.porch;
+    $dataPrice = btn.dataset.price;
+    $dataPriceSq = btn.dataset.priceSq;
+    $dataHouse = btn.dataset.house;
+    $dataJk = btn.dataset.jk;
+    $dataRooms = btn.dataset.rooms;
+    $dataSq = btn.dataset.sq;
+    $dataCond = btn.dataset.cond;
+
+    $('#flatNumber').html($dataFlatNumber);
+    $('#flatPrice').html($dataPrice);
+    $('#flatPriceSq').html($dataPriceSq);
+    $('#flatFloor').html($dataFloor);
+    $('#flatPorch').html($dataPorch);
+    $('#flatHouse').html($dataHouse);
+    $('#flatJk').html($dataJk);
+    $('#flatRooms').html($dataRooms);
+    $('#flatSq').html($dataSq);
+    switch($dataCond)
+    {
+        case('Свободная'):
+            $('#flatCond').append($condFree); 
+            break;
+        case('Забронированная'):
+            $('#flatCond').append($condBooked);   
+            break;
+        case('Проданная'):
+            $('#flatCond').append($condSelled);   
+            break;
+        case('Недоступна'):
+            $('#flatCond').append($condDis);  
+            break;
+    }
+}
+
+$(document).ready(function() {
+    $('.sideMenu__close').on('click', function(){
+        $sideMenu.hide(400); 
+     });
+
+    $('.section-floor__cell').on('click', function(){
+    if($sideMenu.css('display') != 'none' )
+    {
+        insertData($sideMenu, this);
+    }
+    else
+    {
+        $sideMenu.show(400);
+        insertData($sideMenu, this); 
+    }
+});
+});
+
